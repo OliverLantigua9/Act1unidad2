9d9f4c0f93fa5b710a7e0f0328140fa6e96bbc1f
OliverLantigua9 <97769726+OliverLantigua9@users.noreply.github.com>
2025-06-14 10:38:13 -0400

    primer ejercicio

---
+++ Ejercicio1/Controllers/WelcomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WelcomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetWelcomeMessage(string name)
+        {
+            return Ok(string.Format("¡Hola {0}! Bienvenido a mi API", name));
+        }
+    }
+}
